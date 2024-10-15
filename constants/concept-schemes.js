5fae57734e180414500dbdb01f9ff52a99f7facd
Achraf <achrafatauil@hotmail.com>
2024-10-15 21:39:58 +0200

    seperate constants in own files in constants folder

---
+++ constants/concept-schemes.js
@@ -0,0 +1,8 @@
+export const CONCEPT_SCHEMES = [
+  "http://lblod.data.gift/concept-schemes/5979ff3e-1f4c-4271-ae40-e1d2000d6412",   // Subsidy applications
+  "http://data.vlaanderen.be/id/conceptscheme/BestuurseenheidClassificatieCode",   // Bestuurseenheid classificaties
+  "http://lblod.data.gift/concept-schemes/f7274d7f-31d5-4b02-aca8-b96481115651",   // Subsidy application statuses
+  "http://data.lblod.info/id/conceptscheme/EconomischeActoren",                    // Economische Actoren
+  "http://data.lblod.info/id/conceptscheme/CombinedOrganisationClassification",    // Combine economische actoren and bestuurseenheiden
+  "http://data.lblod.info/id/conceptscheme/ACMIDMOrganisationClassification"       // Economische Actoren
+];
